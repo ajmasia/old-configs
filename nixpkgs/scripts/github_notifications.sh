e479d985d1e5b4a2229e128be58c4521cfeb9073
Antonio José Masiá <email@antoniomasia.com>
2022-01-25 07:54:16 +0100

    wip: improve polybar

---
+++ nixpkgs/scripts/github_notifications.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+USER="ajmasia"
+TOKEN=$GH_TOKEN
+
+notifications=$(echo "user = \"$USER:$TOKEN\"" | curl -sf -K- https://api.github.com/notifications | jq ".[].unread" | grep -c true)
+
+if [ "$notifications" -gt 0 ]; then
+    echo "$notifications"
+else
+    echo "0"
+fi
+
